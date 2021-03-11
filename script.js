45d4742508938fb26ac53e66aa9d2a273fc23cdb
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-03-11 19:24:28 -0300

    Tentativas frustradas de solucionar o setimo requisito

--- script.js
+++ script.js
@@ -16,9 +16,6 @@ function criaTarefa() {
 
 botãoCriar.addEventListener('click', criaTarefa);
 
-function colorList() {
-   
-}
 /* https://pt.stackoverflow.com/questions/52418/como-limpar-campos-de-formulario-html
 https://pt.stackoverflow.com/questions/173221/como-checar-se-uma-string-est%C3%A1-vazia-em-javascript/173226
 https://developer.mozilla.org/pt-BR/docs/Web/API/Document/createElement */
