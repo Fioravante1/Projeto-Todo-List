fe3876a4a25e2418638aff369cf5006c4b188dc3
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-03-11 11:38:25 -0300

    criando funçao para limpar o input

---
+++ script.js
@@ -0,0 +1,9 @@
+const textoUsuario = document.querySelectorAll('#texto-tarefa');
+const listaTarefa = document.querySelectorAll('#lista-tarefas');
+const botãoCriar = document.querySelectorAll('#criar-tarefa');
+
+function limparTarefa() {
+  const textoVazio = '';
+  if (textoUsuario !== textoVazio);
+  textoUsuario.value = '';
+}
