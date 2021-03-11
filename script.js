862f7bd055975fa1b476bc4c832de4d92d459d01
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-03-11 17:39:46 -0300

    Eliminando uma função não necessaria

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 const listaTarefa = document.getElementById('lista-tarefas');
 const botãoCriar = document.getElementById('criar-tarefa');
+const elementoUsuario = document.getElementById('texto-tarefa');
 
 function criaTarefa() {
-  const elementoUsuario = document.getElementById('texto-tarefa');
-  const novaTarefa = document.createElement('li');
-  listaTarefa.appendChild(novaTarefa).innerHTML = elementoUsuario.value;
-  elementoUsuario.value = '';
+  if (elementoUsuario.value === '') {
+    alert('Adicione uma tarefa!');
+    elementoUsuario.focus();
+  } else {
+    const novaTarefa = document.createElement('li');
+    listaTarefa.appendChild(novaTarefa).innerHTML = elementoUsuario.value;
+    elementoUsuario.value = '';
+    elementoUsuario.focus();
+  }
 }
 
-function adicionaTarefa() {
-  botãoCriar.addEventListener('click', criaTarefa);
-}
+botãoCriar.addEventListener('click', criaTarefa);
 
-adicionaTarefa();
+function colorList() {
+   
+}
 /* https://pt.stackoverflow.com/questions/52418/como-limpar-campos-de-formulario-html
 https://pt.stackoverflow.com/questions/173221/como-checar-se-uma-string-est%C3%A1-vazia-em-javascript/173226
 https://developer.mozilla.org/pt-BR/docs/Web/API/Document/createElement */
