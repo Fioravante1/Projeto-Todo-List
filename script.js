d5a19bed1f99085e08b297839ff10fce3257954f
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-03-11 13:51:15 -0300

    Mudando as duas primeiras funçoes

--- script.js
+++ script.js
@@ -1,26 +1,17 @@
-const textoUsuario = document.querySelectorAll('#texto-tarefa');
+const textoUsuario = document.querySelectorAll('#texto-tarefa').value;
 const listaTarefa = document.querySelectorAll('#lista-tarefas');
 const botãoCriar = document.querySelectorAll('#criar-tarefa');
 
 function limparTexto() {
-  const textoVazio = '';
-  if (textoUsuario !== textoVazio) {
-    textoUsuario.value = '';
-  }
+  textoUsuario.value = '';
 }
 
 function criaTarefa() {
-    const textoVazio = '';
-    for (let index = 0; index < listaTarefa.length; index += 1) {
-        if (textoUsuario !== textoVazio) {
-            let novaPosição = document.createElement('li');
-            listaTarefa.appendChild(novaPosição);
-        }
-    }
+  const novaTarefa = document.createElement('li');
+  listaTarefa.appendChild(novaTarefa).innerHTML = textoUsuario;
+  limparTexto(); 
 }
 
-
-
-https://pt.stackoverflow.com/questions/52418/como-limpar-campos-de-formulario-html
+/* https://pt.stackoverflow.com/questions/52418/como-limpar-campos-de-formulario-html
 https://pt.stackoverflow.com/questions/173221/como-checar-se-uma-string-est%C3%A1-vazia-em-javascript/173226
-https://developer.mozilla.org/pt-BR/docs/Web/API/Document/createElement
+https://developer.mozilla.org/pt-BR/docs/Web/API/Document/createElement */
