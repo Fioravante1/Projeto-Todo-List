591575002ced8981d17b23e0c3baba0937805412
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-03-11 16:29:49 -0300

    Muda a funcao cria tarefa

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-const textoUsuario = document.querySelectorAll('#texto-tarefa').value;
-const listaTarefa = document.querySelectorAll('#lista-tarefas');
-const botãoCriar = document.querySelectorAll('#criar-tarefa');
-
-function limparTexto() {
-  textoUsuario.value = '';
-}
+const listaTarefa = document.getElementById('lista-tarefas');
+const botãoCriar = document.getElementById('criar-tarefa');
 
 function criaTarefa() {
+  const elementoUsuario = document.getElementById('texto-tarefa');
   const novaTarefa = document.createElement('li');
-  listaTarefa.appendChild(novaTarefa).innerHTML = textoUsuario;
-  limparTexto(); 
+  listaTarefa.appendChild(novaTarefa).innerHTML = elementoUsuario.value;
+  elementoUsuario.value = '';
 }
 
+function adicionaTarefa() {
+  botãoCriar.addEventListener('click', criaTarefa);
+}
+
+adicionaTarefa();
 /* https://pt.stackoverflow.com/questions/52418/como-limpar-campos-de-formulario-html
 https://pt.stackoverflow.com/questions/173221/como-checar-se-uma-string-est%C3%A1-vazia-em-javascript/173226
 https://developer.mozilla.org/pt-BR/docs/Web/API/Document/createElement */
