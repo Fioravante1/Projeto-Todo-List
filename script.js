0f6bb6a3d82931b2cbd7a7e50a3b57559bbcc81d
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-03-11 20:50:08 -0300

    Cria funçao para pintar o item da lista

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const listaTarefa = document.getElementById('lista-tarefas');
 const botãoCriar = document.getElementById('criar-tarefa');
 const elementoUsuario = document.getElementById('texto-tarefa');
-
+/* Função criada com ajuda dos colegas dos Plantoes. Com a ajuda do racionio de Camila
+ Arruda e Issac foi posivel desenvolver o trecho do codigo onde adiciona tarefas a lista.
+ Referencias de sites no final do codigo. */
 function criaTarefa() {
   if (elementoUsuario.value === '') {
     alert('Adicione uma tarefa!');
@@ -15,6 +17,17 @@ function criaTarefa() {
 }
 
 botãoCriar.addEventListener('click', criaTarefa);
+
+const corCinza = document.getElementsByClassName('corFundo');
+function corFundo() {
+  if (corCinza) {
+    listaTarefa.classList.add('corFundo');
+  } else {
+    listaTarefa.classList.remove('corFundo');
+  }
+}
+
+listaTarefa.addEventListener('click', corFundo);
 
 /* https://pt.stackoverflow.com/questions/52418/como-limpar-campos-de-formulario-html
 https://pt.stackoverflow.com/questions/173221/como-checar-se-uma-string-est%C3%A1-vazia-em-javascript/173226
